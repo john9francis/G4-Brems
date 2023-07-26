dec188c12800300eea45e6998e9323db0c449d50
John Francis <john9francis@gmail.com>
2023-07-26 16:33:37 -0600

    created cmake project

---
+++ G4-Brems.cpp
@@ -0,0 +1,12 @@
+﻿// G4-Brems.cpp : Defines the entry point for the application.
+//
+
+#include "G4-Brems.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
