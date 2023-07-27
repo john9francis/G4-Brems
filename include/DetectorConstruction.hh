6e7301e6ecd2279188e05caadda8a30112c188ee
John Francis <john9francis@gmail.com>
2023-07-27 16:46:51 -0600

    started most basic detector construction class

--- include/DetectorConstruction.hh
+++ include/DetectorConstruction.hh
@@ -7,7 +7,11 @@ namespace G4_BREMS
 {
 	class DetectorConstruction : public G4VUserDetectorConstruction
 	{
+	public:
+		DetectorConstruction() = default;
+		~DetectorConstruction() override = default;
 
+		G4VPhysicalVolume* Construct() override;
 	};
 }
 
--- src/DetectorConstruction.cc
+++ src/DetectorConstruction.cc
@@ -3,6 +3,9 @@
 
 namespace G4_BREMS
 {
-
+	G4VPhysicalVolume* DetectorConstruction::Construct()
+	{
+		// construct our detectors here
+	}
 
 }
