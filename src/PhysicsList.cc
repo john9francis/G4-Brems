205bb11c52f9abde01495bd3919c0d540cde76bc
John Francis <john9francis@gmail.com>
2023-08-03 11:32:49 -0600

    Added bremsstrahlung to the physics list

--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -1,14 +1,28 @@
 #include "PhysicsList.hh"
 
+#include "G4ParticleDefinition.hh"
+#include "G4ProcessManager.hh"
+#include "G4Electron.hh"
+#include "G4eBremsstrahlung.hh"
+
 namespace G4_BREMS
 {
 	PhysicsList::PhysicsList() : G4VModularPhysicsList() {}
 	PhysicsList::~PhysicsList() {}
 
-	PhysicsList::ConstructParticle() {
+	void PhysicsList::ConstructParticle() {
 		// Construct particle here
+		G4Electron::ElectronDefinition();
+		
 	}
-	PhysicsList::ConstructProcess() {
-		// Construct process here
+	void PhysicsList::ConstructProcess() {
+		// Construct processes here
+		G4ProcessManager* electronProcessManager = G4Electron::Electron()->GetProcessManager();
+	
+		// Creating Bremsstrahlung process
+		G4eBremsstrahlung* bremsProcess = new G4eBremsstrahlung();
+
+		// add it to the electrons processes
+		electronProcessManager->AddProcess(bremsProcess);
 	}
 }
