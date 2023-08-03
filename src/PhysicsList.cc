5d1c5c272ce94a730eb292ae81e0a27c0c17d2fc
John Francis <john9francis@gmail.com>
2023-08-03 11:23:49 -0600

    implemented basics for physicslist.cc

---
+++ src/PhysicsList.cc
@@ -0,0 +1,14 @@
+#include "PhysicsList.hh"
+
+namespace G4_BREMS
+{
+	PhysicsList::PhysicsList() : G4VModularPhysicsList() {}
+	PhysicsList::~PhysicsList() {}
+
+	PhysicsList::ConstructParticle() {
+		// Construct particle here
+	}
+	PhysicsList::ConstructProcess() {
+		// Construct process here
+	}
+}
