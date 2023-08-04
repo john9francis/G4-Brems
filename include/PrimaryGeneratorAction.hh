f0647a973dc29310aad25f5a723943e81ea31b19
John Francis <john9francis@gmail.com>
2023-08-04 14:44:14 -0600

    set up a basic primary generator action for the 6 MeV electrons. Next step is create action initialization to put the primarygeneratoraction into the runmanager.

--- include/PrimaryGeneratorAction.hh
+++ include/PrimaryGeneratorAction.hh
@@ -3,15 +3,25 @@
 
 
 #include "G4VUserPrimaryGeneratorAction.hh"
+#include "G4ThreeVector.hh"
+#include "globals.hh"
+#include "G4SystemOfUnits.hh"
+#include "G4ParticleGun.hh"
+
 
 
 namespace G4_BREMS
 {
+
 	class PrimaryGeneratorAction : public G4VUserPrimaryGeneratorAction
 	{
 	public:
+		PrimaryGeneratorAction();
+		~PrimaryGeneratorAction();
 
-		void GeneratePrimaries(G4Event*) override;
+		virtual void GeneratePrimaries(G4Event*);
+
+		G4ParticleGun* fParticleGun;
 	};
 }
 
--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -1,9 +1,39 @@
 #include "PrimaryGeneratorAction.hh"
 
+#include "G4Event.hh"
+#include "G4ParticleTable.hh"
+#include "G4ParticleDefinition.hh"
+
 namespace G4_BREMS
 {
+	PrimaryGeneratorAction::PrimaryGeneratorAction() {
+		// set up particle gun
+		G4int nParticles = 1;
+		fParticleGun = new G4ParticleGun(nParticles);
+
+		// define particle properties
+		const G4String& particleName = "electron";
+		G4double energy = 6. * MeV;
+		G4ThreeVector position = G4ThreeVector(0, 0, -1 * cm);
+		G4ThreeVector momentumDirection = G4ThreeVector(0, 0, 1);
+
+		// default particle kinematic
+		G4ParticleTable* particleTable = G4ParticleTable::GetParticleTable();
+		G4ParticleDefinition* particle
+			= particleTable->FindParticle(particleName);
+		fParticleGun->SetParticleDefinition(particle);
+		fParticleGun->SetParticleEnergy(energy);
+		fParticleGun->SetParticlePosition(position);
+		fParticleGun->SetParticleMomentumDirection(momentumDirection);
+	}
+
+	PrimaryGeneratorAction::~PrimaryGeneratorAction() {
+		delete fParticleGun;
+	}
+
 	void PrimaryGeneratorAction::GeneratePrimaries(G4Event* event)
 	{
 		// satisfy "generate primaries" here.
+		fParticleGun->GeneratePrimaryVertex(event);
 	}
 }
