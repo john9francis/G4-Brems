62ca9bc8ccb75b487e4a0fedfec149b75c28865d
John Francis <john9francis@gmail.com>
2023-08-08 11:33:59 -0600

    set up a very simple hit class

--- include/Hit.hh
+++ include/Hit.hh
@@ -3,5 +3,17 @@
 
 // My own Hit class
 
+#include "G4VHit.hh"
+
+namespace G4_BREMS {
+	class Hit : public G4VHit {
+	public:
+		Hit();
+		~Hit();
+
+		//void Draw() override;
+		void Print() override;
+	};
+}
 
 #endif // !G4_BREMS_HIT
--- src/Hit.cc
+++ src/Hit.cc
@@ -1,3 +1,14 @@
 
 #include "Hit.hh"
 
+using namespace std;
+
+namespace G4_BREMS {
+Hit::Hit() {}
+Hit::~Hit() {}
+
+void Hit::Print() {
+	std::cout << "Hit!" << '\n';
+}
+
+}
