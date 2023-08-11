94b4301fe2e6be5d22efa7a3202eec2f5bb7db52
John Francis <john9francis@gmail.com>
2023-08-11 13:36:32 -0600

    working on implementing steppingaction. So far we are saving the volume that the particle is in for each step. Next we will check if this step happens to be our detector.

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -1,13 +1,25 @@
 
 #include "SteppingAction.hh"
+#include "G4Step.hh"
 
 namespace G4_BREMS {
 	SteppingAction::SteppingAction() {
 
 	}
 
-	void SteppingAction::UserSteppingAction(const G4Step*) {
+	void SteppingAction::UserSteppingAction(const G4Step* step) {
 		// Register hit if the step is inside the tracking volume
+		
+		// get volume of the current step (borrowed from B1)
+		// ps. this is dynamically allocated
+		G4LogicalVolume* volume
+			= step->GetPreStepPoint()->GetTouchableHandle()
+			->GetVolume()->GetLogicalVolume();
+
+		// if the volume is our gamma detector, create a hit.
+		// else, move on. 
+		//if (volume = )
+
 	}
 	
 }
