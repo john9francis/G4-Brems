75367044af650201eb572ccd6f98da4a72681dbb
John Francis <john9francis@gmail.com>
2023-08-14 13:50:47 -0600

    added runaction to the action init

--- src/Actioninit.cc
+++ src/Actioninit.cc
@@ -2,6 +2,7 @@
 #include "ActionInit.hh"
 #include "PrimaryGeneratorAction.hh"
 #include "SteppingAction.hh"
+#include "RunAction.hh"
 
 namespace G4_BREMS {
 
@@ -10,6 +11,7 @@ namespace G4_BREMS {
 		// set the Geant4 actions
 		SetUserAction(new PrimaryGeneratorAction);
 		SetUserAction(new SteppingAction);
+		SetUserAction(new RunAction);
 	};
 
 }
