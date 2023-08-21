2eee07ef958225d034d7e56c1e7fb367ff30b269
John Francis <john9francis@gmail.com>
2023-08-21 13:05:39 -0600

    Created runaction class

---
+++ include/RunAction.hh
@@ -0,0 +1,10 @@
+#ifndef G4_BREMS_RUN_ACTION_HH
+#define G4_BREMS_RUN_ACTION_HH
+
+#include "G4UserRunAction.hh"
+
+class RunAction : G4UserRunAction {
+
+};
+
+#endif // !G4_BREMS_RUN_ACTION_HH
---
+++ src/RunAction.cc
@@ -0,0 +1,3 @@
+
+#include "RunAction.hh"
+
