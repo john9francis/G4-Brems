4be44edc9e067ad55c172bf2929253e71445d70a
John Francis <john9francis@gmail.com>
2023-08-21 13:27:47 -0600

    set up runaction with a hitscollection

--- include/RunAction.hh
+++ include/RunAction.hh
@@ -3,6 +3,9 @@
 
 #include "G4UserRunAction.hh"
 
+#include "G4THitsCollection.hh"
+#include "Hit.hh"
+
 namespace G4_BREMS {
 
 	class RunAction : G4UserRunAction {
@@ -12,6 +15,10 @@ namespace G4_BREMS {
 
 		void BeginOfRunAction(const G4Run* aRun) override;
 		void EndOfRunAction(const G4Run* aRun) override;
+	};
+
+	class HitsCollection : G4THitsCollection<Hit> {
+
 	};
 
 
