a304b09a7bc9470c59963629d928a283e4d0d8b5
John Francis <john9francis@gmail.com>
2023-09-15 21:15:43 -0600

    fixed a bug where you couldn't just /run/beamOn 100 or whatever. Now if you run your own simulation, the data saves to the NTuples output file, and if you run a specific mac file, it will save to a different file with the appropriate name.

--- src/RunAction.cc
+++ src/RunAction.cc
@@ -33,6 +33,10 @@ namespace G4_BREMS {
 	void RunAction::BeginOfRunAction(const G4Run* aRun) {
 		// start time
 		fTimer.Start();
+
+		auto analysisManager = G4AnalysisManager::Instance();
+
+		analysisManager->OpenFile();
 	}
 
 	void RunAction::EndOfRunAction(const G4Run* aRun) {
@@ -40,7 +44,8 @@ namespace G4_BREMS {
 		auto analysisManager = G4AnalysisManager::Instance();
 
 		// write to output file
-		//analysisManager->Write();
+		analysisManager->Write();
+		analysisManager->CloseFile();
 
 		// end time
 		fTimer.Stop();
