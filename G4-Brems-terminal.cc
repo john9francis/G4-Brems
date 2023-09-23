c69716b07c7749a59df7d4f7c0faf1b4f5b82e2c
John Francis <john9francis@gmail.com>
2023-09-23 10:18:04 -0600

    created an alternative entrypoint, 'g4-brems-terminal.cc' that creates it's own executable and opens g4-brems in interactive mode for super fast use

---
+++ G4-Brems-terminal.cc
@@ -0,0 +1,35 @@
+// An alternative entrypoint that opens the project in terminal mode.
+
+#include "G4RunManagerFactory.hh"
+#include "G4UIsession.hh"
+#include "G4UIterminal.hh"
+
+#include "DetectorConstruction.hh"
+#include "PhysicsList.hh"
+#include "ActionInit.hh"
+
+using namespace G4_BREMS;
+
+int main(int argc, char** argv)
+{
+	// create default runmanager
+	auto* runManager =
+		G4RunManagerFactory::CreateRunManager(G4RunManagerType::Default);
+
+	// set 3 required initialization classes
+	runManager->SetUserInitialization(new DetectorConstruction());
+	runManager->SetUserInitialization(new PhysicsList());
+	runManager->SetUserInitialization(new ActionInit());
+
+	// Initialize G4 kernel
+	runManager->Initialize();
+
+	// Define UI terminal for interactive mode
+	G4UIsession* session = new G4UIterminal;
+	session->SessionStart();
+	delete session;
+
+	// job termination
+	delete runManager;
+	return 0;
+}
