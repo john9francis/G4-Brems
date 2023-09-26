1aab6704557bb388cf10c8c3f7522705f0dfa14a
John Francis <john9francis@gmail.com>
2023-09-26 00:34:59 -0600

    still testing physics lists

--- src/PhysicsList.cc
+++ src/PhysicsList.cc
@@ -16,7 +16,8 @@ namespace G4_BREMS
 	PhysicsList::PhysicsList() : G4VModularPhysicsList() {
 		// Get the standard EM particles
 		//RegisterPhysics(new G4EmStandardPhysics());
-		RegisterPhysics(new G4EmStandardPhysics_option1);
+		//RegisterPhysics(new G4EmStandardPhysics_option1);
+		RegisterPhysics(new G4EmStandardPhysics_option2);
 	}
 	PhysicsList::~PhysicsList() {}
 
