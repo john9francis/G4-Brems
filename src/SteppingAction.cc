54b74a7d8f000d9f7caa425e1e6cbf251145f5a3
John Francis <john9francis@gmail.com>
2023-10-26 18:40:46 -0600

    Directly sending the energy over to the analysis right from stepping action. Now I can decouple stepping action from the event action

--- src/SteppingAction.cc
+++ src/SteppingAction.cc
@@ -5,6 +5,7 @@
 #include "G4Step.hh"
 #include "G4RunManager.hh"
 
+#include "G4AnalysisManager.hh"
 
 namespace G4_BREMS {
 	SteppingAction::SteppingAction(EventAction* eventAction) {
@@ -38,17 +39,20 @@ namespace G4_BREMS {
 		// get the first secondary, it should be a gamma
 		const G4Track* track = (*secondaries)[0];
 
+		// Check particle name
 		const G4ParticleDefinition* particle = track->GetParticleDefinition();
-
 		G4String particleName = particle->GetParticleName();
-
-
 		if (particleName != "gamma") { return; }
 
 
+		// if it's a bremsstrahlung gamma, send the energy over to analysis
 		G4double energy = track->GetTotalEnergy();
-		//G4cout << "Secondary energy: " << G4BestUnit(energy, "Energy") << G4endl;
-		feventAction->AddEnergy(energy);
+
+		auto analysisManager = G4AnalysisManager::Instance();
+		analysisManager->FillNtupleDColumn(0, energy);
+		analysisManager->AddNtupleRow();
+
+		//feventAction->AddEnergy(energy);
 
 
 	}
